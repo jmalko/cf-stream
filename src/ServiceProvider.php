e7c74f86a33bc7a724f7b16a277490aaf8b9f7d0
Jonathan Malko <jon.malko@gmail.com>
2023-08-30 12:17:54 -0400

    ensure creator_id optional

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -31,11 +31,13 @@ class ServiceProvider extends AddonServiceProvider
         $this->registerCpRoutes(function () {
 
             Route::get('videos', function() {
-                
+
                 $curl = curl_init();
 
+                $params = $this->creator_id ? "?creator=$this->creator_id" : "";
+
                 curl_setopt_array($curl, [
-                  CURLOPT_URL => "https://api.cloudflare.com/client/v4/accounts/$this->account_id/stream/?creator=$this->creator_id",
+                  CURLOPT_URL => "https://api.cloudflare.com/client/v4/accounts/$this->account_id/stream/$params",
                   CURLOPT_RETURNTRANSFER => true,
                   CURLOPT_ENCODING => "",
                   CURLOPT_MAXREDIRS => 10,
@@ -44,7 +46,7 @@ class ServiceProvider extends AddonServiceProvider
                   CURLOPT_CUSTOMREQUEST => "GET",
                   CURLOPT_HTTPHEADER => [
                     "Content-Type: application/json",
-                    "Authorization: bearer $this->api_key"    
+                    "Authorization: bearer $this->api_key"
                   ],
                 ]);
 
@@ -59,26 +61,26 @@ class ServiceProvider extends AddonServiceProvider
 
                 } else {
                     $response = json_decode($response, true);
-    
+
                     $videos = $response['result'];
-    
+
                     return view("cf-stream::index", compact('videos'));
                 }
 
             })->name('video.index');
         });
-     
+
         $this->registerWebRoutes(function () {
-     
+
         });
-     
+
         $this->registerActionRoutes(function () {
 
             Route::get('list', function() {
 
             })->name('video.index');
 
-            Route::post('upload', function(){ 
+            Route::post('upload', function(){
 
                 $request = request();
 
@@ -86,61 +88,61 @@ class ServiceProvider extends AddonServiceProvider
 
                 curl_setopt_array($curl, [
                     CURLOPT_URL => "https://api.cloudflare.com/client/v4/accounts/$this->account_id/stream?direct_user=true",
-                    CURLOPT_RETURNTRANSFER => true,    
-                    CURLOPT_ENCODING => "",    
-                    CURLOPT_MAXREDIRS => 10,    
-                    CURLOPT_TIMEOUT => 30,    
-                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,    
-                    CURLOPT_CUSTOMREQUEST => "POST",    
-                    CURLOPT_POSTFIELDS => "",    
-                    CURLOPT_HEADER => true,    
-                    CURLOPT_NOBODY => true,    
-                    CURLOPT_HTTPHEADER => [    
-                        "Content-Type: application/json",    
-                        "Tus-Resumable: 1.0.0",    
-                        "Upload-Length: " . $request->header('Upload-Length'),    
-                        "Upload-Metadata: " . $request->header('Upload-Metadata'),    
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => "",
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 30,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => "POST",
+                    CURLOPT_POSTFIELDS => "",
+                    CURLOPT_HEADER => true,
+                    CURLOPT_NOBODY => true,
+                    CURLOPT_HTTPHEADER => [
+                        "Content-Type: application/json",
+                        "Tus-Resumable: 1.0.0",
+                        "Upload-Length: " . $request->header('Upload-Length'),
+                        "Upload-Metadata: " . $request->header('Upload-Metadata'),
                         "Upload-Creator: $this->creator_id",
-                        "Authorization: bearer $this->api_key"    
-                    ],    
-                ]);    
+                        "Authorization: bearer $this->api_key"
+                    ],
+                ]);
 
-                $response = curl_exec($curl);    
-                $err = curl_error($curl);    
+                $response = curl_exec($curl);
+                $err = curl_error($curl);
 
-                curl_close($curl);    
+                curl_close($curl);
 
-                if ($err) {    
+                if ($err) {
 
-                    echo "cURL Error #:" . $err;    
+                    echo "cURL Error #:" . $err;
 
-                } else {    
-                    // parse response headers from string to array    
-                    $response_headers = [];    
-                    $response_headers_string = substr($response, 0, strpos($response, "\r\n\r\n"));    
-                    foreach (explode("\r\n", $response_headers_string) as $i => $line) {    
-                        if ($i === 0) {    
-                            $response_headers['http_code'] = $line;    
-                        } else {    
-                            list ($key, $value) = explode(': ', $line);    
-                            $response_headers[$key] = $value;    
-                        }    
-                    }    
+                } else {
+                    // parse response headers from string to array
+                    $response_headers = [];
+                    $response_headers_string = substr($response, 0, strpos($response, "\r\n\r\n"));
+                    foreach (explode("\r\n", $response_headers_string) as $i => $line) {
+                        if ($i === 0) {
+                            $response_headers['http_code'] = $line;
+                        } else {
+                            list ($key, $value) = explode(': ', $line);
+                            $response_headers[$key] = $value;
+                        }
+                    }
 
-                    $destination = $response_headers["Location"];    
+                    $destination = $response_headers["Location"];
 
-                    return response()    
-                        ->noContent()    
-                        ->header('Access-Control-Expose-Headers', 'Location')    
-                        ->header('Access-Control-Allow-Headers', '*')    
-                        ->header('Access-Control-Allow-Origin', '*')    
-                        ->header('Location', $destination);    
-                    } 
+                    return response()
+                        ->noContent()
+                        ->header('Access-Control-Expose-Headers', 'Location')
+                        ->header('Access-Control-Allow-Headers', '*')
+                        ->header('Access-Control-Allow-Origin', '*')
+                        ->header('Location', $destination);
+                    }
 
                 })
                 // had to disable CSRF token verification for this route because it sends CSRF on the PATCH request to Cloudflare as well - fails it's CORS
-                ->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]) 
-                ->name('videos.upload'); 
+                ->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class])
+                ->name('videos.upload');
 
             });
     }
